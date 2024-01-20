890cd9acca69d4e596384b2132a304f77250226d
LukeMech <lukaszjajkommz@gmail.com>
2024-01-20 16:03:50 +0100

    Bugfix

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,9 @@
 rm -rf ../img/old
-mkdir -p ../img/old
-mv ../img/* ../img/old/
+mkdir -p ../img/old/prompts
+mv ../img/*.img ../img/old/
+mv ../img/prompts/* ../img/old/prompts/
 
+cd ..
 git config --global user.name cleanup
 git config --global user.email github-actions@github.com
 git add .
