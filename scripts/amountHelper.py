7711ea7e9fdaca4799076defa4109262ef188948
LukeMech <lukaszjajkommz@gmail.com>
2024-01-21 00:15:40 +0100

    Lessgo

---
+++ scripts/amountHelper.py
@@ -0,0 +1,11 @@
+import sys, json, subprocess
+
+if len(sys.argv) != 2:
+    print("Multiplier amount not found!")
+    sys.exit(1)
+
+json_array = [i for i in range(int(sys.argv[1]))]
+json_variable = json.dumps(json_array).replace(" ", "")
+
+subprocess.run(f'echo Exporting amount={json_variable}', shell=True)
+subprocess.run(f'echo amount={json_variable} >> $GITHUB_OUTPUT', shell=True)
