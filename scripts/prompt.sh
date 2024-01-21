dedc9d73d32f2adb91735fdddff0757a13717009
LukeMech <lukaszjajkommz@gmail.com>
2024-01-21 10:09:54 +0100

    lessgoo

--- scripts/prompt.sh
+++ scripts/prompt.sh
@@ -1,9 +1,23 @@
 pip install -U pip
-pip install -U g4f~=0.2.0.3
+
+if [ "$EXPERIMENTAL" == true ]; then
+    pip install -U tensorflow~=2.15.0.post1 
+    pip install -U transformers~=4.36.2
+else
+    pip install -U g4f~=0.2.0.3
+fi
 
 mkdir -p ./tmp
-if [ -n "$PROMPT" ]; then
-    python prompt.py $CONFIGPATH "$PROMPT"
-else 
-    python prompt.py $CONFIGPATH
+if [ "$EXPERIMENTAL" == true ]; then
+    if [ -n "$PROMPT" ]; then
+        python experimental-prompt.py $CONFIGPATH "$PROMPT"
+    else 
+        python experimental-prompt.py $CONFIGPATH 
+    fi
+else
+    if [ -n "$PROMPT" ]; then
+        python prompt.py $CONFIGPATH "$PROMPT"
+    else 
+        python prompt.py $CONFIGPATH
+    fi
 fi
