b1424c1cfea9e2a2614a7d9c782cead05c42dc5e
LukeMech <lukaszjajkommz@gmail.com>
2024-01-21 03:21:10 +0100

    xd

--- scripts/modelsHelper.py
+++ scripts/modelsHelper.py
@@ -16,3 +16,4 @@ json_variable = json.dumps(quoted_list).replace(" ", "")
 
 subprocess.run(f'echo models={json_variable} >> $GITHUB_OUTPUT', shell=True)
 subprocess.run(f'echo models={json_variable}', shell=True)
+subprocess.run(f'export models={json_variable}', shell=True)
