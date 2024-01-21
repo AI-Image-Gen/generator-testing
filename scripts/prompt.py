3cb562df590e5e9451ca233c863e29486667ac24
LukeMech <lukaszjajkommz@gmail.com>
2024-01-21 01:37:31 +0100

    xD

--- scripts/prompt.py
+++ scripts/prompt.py
@@ -1,4 +1,4 @@
-import g4f, json, sys
+import g4f, json, sys, os
 
 if len(sys.argv) != 2 and len(sys.argv) != 3:
     print("Json config file path missing!")
@@ -10,11 +10,20 @@ with open(sys.argv[1] + '/langModel.json', 'r') as file:
 with open(sys.argv[1] + '/usedPrompts.json', 'r') as file:
     usedPrompts = json.load(file)
 
-ctx = data["prompt"] + " It must be EXACTLY AND ONLY 1 PROMPT, FORMATTED TO BE BETWEEN \" MARKS. You can't use these prompts, find something new and interesting, not really connected with these subjects: " + '\n' + '\n'.join(usedPrompts)
+all_justusedprompts = []
+justusedprompts = os.listdir('.')
+justusedprompts_list = [file for file in justusedprompts if file.endswith(".txt")]
+for txt_file in justusedprompts_list:
+    file_path = os.path.join('.', txt_file)  # Construct the full file path
+    with open(file_path, 'r') as file:
+        prompt = file.read()
+        all_justusedprompts.append(prompt)
+
+ctx = data["prompt"] + " It must be EXACTLY AND ONLY 1 PROMPT, FORMATTED TO BE BETWEEN \" MARKS. You can't use these prompts: " + '\n' + '\n'.join(usedPrompts) + '\n' + '\n'.join(all_justusedprompts)
 
 # Enhance non-auto prompt
 if len(sys.argv) == 3:
-    ctx = "Give me 1 nice prompt for ai image generator, that needs to generate \"" + sys.argv[2] + "\". It must be EXACTLY AND ONLY 1 PROMPT, FORMATTED TO BE BETWEEN \" MARKS."
+    ctx = "Give me 1 nice prompt for ai image generator, that needs to generate \"" + sys.argv[2] + "\". It must be EXACTLY AND ONLY 1 PROMPT, FORMATTED TO BE BETWEEN \" MARKS. You can't use these prompts:" + '\n' + '\n'.join(all_justusedprompts)
 
 print('Generating output for question: ' + ctx)
 
