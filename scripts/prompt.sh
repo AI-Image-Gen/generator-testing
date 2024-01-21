6fce6f394c550648100e0a6a2c001c8395107f78
LukeMech <lukaszjajkommz@gmail.com>
2024-01-21 10:20:50 +0100

    xD

--- scripts/prompt.sh
+++ scripts/prompt.sh
@@ -1,7 +1,7 @@
 pip install -U pip
 
 if [ -n "$EXPERIMENTAL" ]; then
-    pip install -U transformers~=4.36.2 tiktoken~=0.5.2
+    pip install -U transformers~=4.36.2 tiktoken~=0.5.2 torch~=2.1.2
 else
     pip install -U g4f~=0.2.0.3
 fi
