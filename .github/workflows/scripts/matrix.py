aeb4f142853b3678aedd688f71c0eec614cebdab
LukeMech <lukaszjajkommz@gmail.com>
2024-01-21 20:24:48 +0100

    Bugfix #1

--- .github/workflows/scripts/matrix.py
+++ .github/workflows/scripts/matrix.py
@@ -8,12 +8,17 @@ REPO_DIR = getenv('REPO_DIR')
 CONFIG_FOLDER = getenv('CONFIG_FOLDER')
 IS_IMAGE = getenv('IS_IMAGE')
 
-if FAST_MODE.lower() == "true" or IS_IMAGE.lower() == "true" or int(RUNNERS_AMOUNT) < 1:
+try:
+    int_runners_amount = int(RUNNERS_AMOUNT)
+except ValueError:
+    int_runners_amount = 0
+
+if FAST_MODE.lower() == "true" or IS_IMAGE.lower() == "true" or int_runners_amount < 1:
     RUNNERS_AMOUNT=1
-elif int(RUNNERS_AMOUNT) > 10:
+elif int_runners_amount > 10:
     RUNNERS_AMOUNT=10
     
-json_array = [i for i in range(int(RUNNERS_AMOUNT))]
+json_array = [i for i in range(int_runners_amount)]
 run_amount_array = dumps(json_array).replace(" ", "")
 
 run(f'echo amount={run_amount_array} >> $GITHUB_OUTPUT', shell=True)
