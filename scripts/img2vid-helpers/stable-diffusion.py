2f06bb37e003c882862e00f3b81333182caf85ef
LukeMech <lukaszjajkommz@gmail.com>
2024-02-01 00:35:19 +0100

    Move to diffusers

--- scripts/img2vid-helpers/stable-diffusion.py
+++ scripts/img2vid-helpers/stable-diffusion.py
@@ -1,46 +1,33 @@
-from os import makedirs, getenv, path
-import subprocess, urllib.request, importlib
+from os import getenv, path
+import subprocess
 
 def run(model, image):
     cfg_folder = getenv("CONFIG_FOLDER")
     runnum = getenv("runnum")
-    ai = getenv("ai")
 
     subprocess.run(f"pip install {' '.join(model['packages'])} --extra-index-url {','.join(model['extra_indexes'])}", shell=True)
-
-    from tqdm import tqdm
-    from git import Repo
     
+    import torch
+    from diffusers import StableVideoDiffusionPipeline
+    from diffusers.utils import load_image, export_to_video
+
     print('\nGenerating video', flush=True)
     print("| Using:", flush=True)
-    print("Model from: " + model["dld_url"], flush=True)
+    print("Model: " + model["model"], flush=True)
 
-    makedirs("./checkpoints/x", exist_ok=True)
-    ext=model["dld_url"].split(".")[-1]
-    with urllib.request.urlopen(model["dld_url"]) as response, open('./checkpoints/'+ ai+"."+ext, 'wb') as output_file:
-        print('Downloading [' + model["dld_url"] + "]...", flush=True)
-        # Get the total file size in bytes
-        file_size = int(response.getheader('Content-Length', 0))
-        # Initialize the tqdm progress bar
-        progress_bar = tqdm(total=file_size, unit='B', unit_scale=True)
-        # Download and write to the local file with progress update
-        while True:
-            buffer = response.read(8192)  # Adjust the buffer size as needed
-            if not buffer:
-                break
-            output_file.write(buffer)
-            progress_bar.update(len(buffer))
-        # Close the progress bar
-        progress_bar.close()
-        output_file.write(response.read())
+    savepath = path.join(path.abspath(cfg_folder), 'img2vid', f"{runnum}.mp4")
 
-    savepath = path.join(path.abspath(cfg_folder), 'img2vid')
+    pipe = StableVideoDiffusionPipeline.from_pretrained(
+        model["model"], torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    )
+    pipe.enable_model_cpu_offload()
 
-    repo_url = 'https://github.com/Stability-AI/generative-models.git'
-    Repo.clone_from(repo_url, path.join(cfg_folder, 'repo'))
-    subprocess.run(f'mv {path.join(cfg_folder, "repo", "*")} .', shell=True)
+    # Load the conditioning image
+    image = load_image(image)
+    image = image.resize((1024, 576))
+
+    generator = torch.Generator(device="cpu")
+    frames = pipe(image, decode_chunk_size=8, num_inference_steps=model["inference_count"], generator=generator).frames[0]
+    export_to_video(frames, savepath, fps=model["fps"])
     
-    helper = importlib.import_module("scripts.sampling.simple_video_sample")
-    helper.sample(image, version="svd_xt", device="cpu", output_folder=savepath, num_steps=model["inference_count"], num_frames=model["frames_count"])
-
     return savepath
--- scripts/img2vid.py
+++ scripts/img2vid.py
@@ -19,25 +19,13 @@ if config["img2img"]["active"]:
     config = json.loads(json.dumps(config).replace("{img2img.out}", path.join(cfg_folder, 'img2img', f'{runnum}.jpg')))
 
 config = config["img2vid"]
-
-run("pip install pillow~=10.2.0", shell=True)
-from PIL import Image
-try:
-    image = Image.open(config["image"])
-except:
-    response = urllib.request.urlopen(config["image"])
-    data = response.read()
-    image = Image.open(BytesIO(data))
-
-image = image.resize((1024, 576))
-image.save('tmp.jpg')
     
 makedirs(path.join(cfg_folder, "img2vid"), exist_ok=True)
 
 print('\nUsing helper: ' + models[ai]['helper'], flush=True)
 
 helper = importlib.import_module(f"img2vid-helpers.{models[ai]['helper']}")
-path = helper.run(models[ai], 'tmp.jpg')
+path = helper.run(models[ai], config["image"])
 
 run(f'echo out={path} >> $GITHUB_OUTPUT', shell=True)
 print(f'Generated video and saved to {path}')
