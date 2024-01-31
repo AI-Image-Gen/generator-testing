fa6091967b281283269dfbbddd4833e1fc02bfe3
LukeMech <lukaszjajkommz@gmail.com>
2024-01-31 22:22:54 +0100

    fiix

--- scripts/img2vid-helpers/stable-diffusion.py
+++ scripts/img2vid-helpers/stable-diffusion.py
@@ -4,6 +4,7 @@ import subprocess, urllib.request, importlib
 def run(model, image):
     cfg_folder = getenv("CONFIG_FOLDER")
     runnum = getenv("runnum")
+    ai = getenv("ai")
 
     subprocess.run(f"pip install {' '.join(model['packages'])} --extra-index-url {','.join(model['extra_indexes'])}", shell=True)
 
@@ -14,24 +15,24 @@ def run(model, image):
     print("| Using:", flush=True)
     print("Model from: " + model["dld_url"], flush=True)
 
-    # makedirs("./tmp/x", exist_ok=True)
-    # ext=model["dld_url"].split(".")[-1]
-    # with urllib.request.urlopen(model["dld_url"]) as response, open('./tmp/'+"model."+ext, 'wb') as output_file:
-    #     print('Downloading [' + model["dld_url"] + "]...", flush=True)
-    #     # Get the total file size in bytes
-    #     file_size = int(response.getheader('Content-Length', 0))
-    #     # Initialize the tqdm progress bar
-    #     progress_bar = tqdm(total=file_size, unit='B', unit_scale=True)
-    #     # Download and write to the local file with progress update
-    #     while True:
-    #         buffer = response.read(8192)  # Adjust the buffer size as needed
-    #         if not buffer:
-    #             break
-    #         output_file.write(buffer)
-    #         progress_bar.update(len(buffer))
-    #     # Close the progress bar
-    #     progress_bar.close()
-    #     output_file.write(response.read())
+    makedirs("./tmp/x", exist_ok=True)
+    ext=model["dld_url"].split(".")[-1]
+    with urllib.request.urlopen(model["dld_url"]) as response, open('./checkpoints/'+ ai+"."+ext, 'wb') as output_file:
+        print('Downloading [' + model["dld_url"] + "]...", flush=True)
+        # Get the total file size in bytes
+        file_size = int(response.getheader('Content-Length', 0))
+        # Initialize the tqdm progress bar
+        progress_bar = tqdm(total=file_size, unit='B', unit_scale=True)
+        # Download and write to the local file with progress update
+        while True:
+            buffer = response.read(8192)  # Adjust the buffer size as needed
+            if not buffer:
+                break
+            output_file.write(buffer)
+            progress_bar.update(len(buffer))
+        # Close the progress bar
+        progress_bar.close()
+        output_file.write(response.read())
 
     savepath = path.join(path.abspath(cfg_folder), 'img2vid')
 
