1ae60644f653c4e900fd68e9c824bed671962537
LukeMech <lukaszjajkommz@gmail.com>
2024-02-01 18:25:08 +0100

    XD

--- scripts/txt2txt-helpers/t5-transformers.py
+++ scripts/txt2txt-helpers/t5-transformers.py
@@ -3,12 +3,12 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 def run(model, ctx):    
     print('\nGenerating output for question: ' + ctx, flush=True)
 
-    tokenizer = T5Tokenizer.from_pretrained(model["model"], max_new_tokens=1024)
+    tokenizer = T5Tokenizer.from_pretrained(model["model"])
     model = T5ForConditionalGeneration.from_pretrained(model["model"])
 
     input_ids = tokenizer(ctx, return_tensors="pt").input_ids
 
-    outputs = model.generate(input_ids)
+    outputs = model.generate(input_ids, max_new_tokens=1024)
     response = tokenizer.decode(outputs[0])
 
     return response
