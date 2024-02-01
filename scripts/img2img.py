c4b608e931863efb0bb3f6d551d2e628cfad47a5
LukeMech <lukaszjajkommz@gmail.com>
2024-02-01 01:52:12 +0100

    yo

--- scripts/img2img.py
+++ scripts/img2img.py
@@ -20,24 +20,13 @@ if config["txt2txt"]["active"]:
     txt2txt = json.loads(getenv("txt2txt").replace('*', ' '))
     config = json.loads(json.dumps(config).replace("{txt2txt.out}", txt2txt[num]))
 config = config["img2img"]
-
-run("pip install pillow~=10.2.0", shell=True)
-from PIL import Image
-try:
-    image = Image.open(config["image"])
-except:
-    response = urllib.request.urlopen(config["image"])
-    data = response.read()
-    image = Image.open(BytesIO(data))
-
-image.save('tmp.jpg')
     
 makedirs(path.join(cfg_folder, "img2img"), exist_ok=True)
 
 print('\nUsing helper: ' + models[ai]['helper'], flush=True)
 
 helper = importlib.import_module(f"img2img-helpers.{models[ai]['helper']}")
-path = helper.run(models[ai], 'tmp.jpg', config["prompt"], config["width"], config["height"])
+path = helper.run(models[ai], config["image"], config["prompt"], config["width"], config["height"])
 
 run(f'echo out={path} >> $GITHUB_OUTPUT', shell=True)
 print(f'Generated image and saved to {path}')
