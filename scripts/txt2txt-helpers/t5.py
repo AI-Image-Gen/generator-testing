f6f6a038de710bbe34bd2a4032c6111283b830d0
LukeMech <lukaszjajkommz@gmail.com>
2024-02-01 18:38:54 +0100

    hm

--- scripts/txt2txt-helpers/t5.py
+++ scripts/txt2txt-helpers/t5.py
@@ -1,5 +1,4 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
-import re
 
 def run(model, ctx):    
     print('\nGenerating output for question: ' + ctx, flush=True)
@@ -10,14 +9,7 @@ def run(model, ctx):
     input_ids = tokenizer(ctx, return_tensors="pt").input_ids
 
     outputs = model.generate(input_ids, max_new_tokens=4096)
-    response = tokenizer.decode(outputs[0])
-
-    pattern = re.compile(r'<pad>(.*?)</s>')
-    matches = pattern.findall(response)
-    if matches:
-        result = matches[0]
-    else:
-        result = response
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
     print('\n\nResponse: ' + response, flush=True)
     print("\n\nFormatted to: " + result, flush=True)
