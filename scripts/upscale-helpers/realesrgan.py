00972688100fe125bbafa38f62365aca8666be97
LukeMech <lukaszjajkommz@gmail.com>
2024-02-01 01:44:27 +0100

    Repair upscale

--- scripts/upscale-helpers/realesrgan.py
+++ scripts/upscale-helpers/realesrgan.py
@@ -13,6 +13,7 @@ def run(model, image, scale):
     print('\nUpscaling image by ' + str(scale) + 'x', flush=True)
     print("| Using:", flush=True)
     print("Model from: " + model["dld_url"], flush=True)
+    print("Script from: " + model["script"], flush=True)
 
     makedirs("./tmp/x", exist_ok=True)
     ext=model["dld_url"].split(".")[-1]
@@ -33,11 +34,25 @@ def run(model, image, scale):
         progress_bar.close()
         output_file.write(response.read())
 
-    repo_url = 'https://github.com/xinntao/Real-ESRGAN.git'
-    Repo.clone_from(repo_url, './esrgan')
+    with urllib.request.urlopen(model["script"]) as response, open('./tmp/script.py', 'wb') as output_file:
+        print('Downloading [' + model["dld_url"] + "]...", flush=True)
+        # Get the total file size in bytes
+        file_size = int(response.getheader('Content-Length', 0))
+        # Initialize the tqdm progress bar
+        progress_bar = tqdm(total=file_size, unit='B', unit_scale=True)
+        # Download and write to the local file with progress update
+        while True:
+            buffer = response.read(8192)  # Adjust the buffer size as needed
+            if not buffer:
+                break
+            output_file.write(buffer)
+            progress_bar.update(len(buffer))
+        # Close the progress bar
+        progress_bar.close()
+        output_file.write(response.read())
 
     savepath = path.join(path.abspath(cfg_folder), 'img_upscale', f'{runnum}.jpg')
-    subprocess.run(f'python ./esrgan/inference_realesrgan.py -i {image} --model_path ./tmp/model.{ext} -o ./out/ --fp32 -s {str(scale)}', shell=True)
+    subprocess.run(f'python ./tmp/script.py -i {image} --model_path ./tmp/model.{ext} -o ./out/ --fp32 -s {str(scale)}', shell=True)
     subprocess.run(f'mv ./out/*.jpg {savepath}', shell=True)
     
     return savepath
