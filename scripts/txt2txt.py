508848ee26d413e6df8827e8089ac871c2039b66
LukaszMech <81306360+LukeMech@users.noreply.github.com>
2024-02-02 01:17:50 +0100

    Update txt2txt.py

--- scripts/txt2txt.py
+++ scripts/txt2txt.py
@@ -21,47 +21,28 @@ if models[ai_model]['extra_indexes']:
 else:
     run(f"pip install {' '.join(models[ai_model]['packages'])}", shell=True)
 
-for num in range(int(data["global"]["out_amount"])):
-    # Restore variables on every literation
-    with open(path.join(cfg_folder, 'cfg.json'), 'r') as file:
-        data = json.load(file)
+num = range(int(data["global"]["out_amount"]))
 
-    all_justusedprompts = []
-    justusedprompts = listdir(path.join(cfg_folder, 'prompts'))
-    justusedprompts_array = [file for file in justusedprompts if file.endswith(".txt")]
-    for txt_file in justusedprompts_array:
-        file_path = path.join(cfg_folder, 'prompts', txt_file)  # Construct the full file path
-        with open(file_path, 'r') as file:
-            prompt = file.read()
-            all_justusedprompts.append(prompt)
+models_str = json.dumps(models)
+# Add old prompts
+used_prompts_str = ', '.join(p for p in usedPrompts)
+models_str = models_str.replace('{used_prompts}', used_prompts_str)
+models = json.loads(models_str)
 
-    models_str = json.dumps(models)
-    # Add old prompts
-    used_prompts_str = ', '.join(p for p in usedPrompts) + ', ' + ', '.join(p for p in all_justusedprompts)
-    models_str = models_str.replace('{used_prompts}', used_prompts_str)
-    models = json.loads(models_str)
+models_str = models_str.replace('{used_prompts_template}', models[ai_model]["used_prompts_template"])
+models = json.loads(models_str)
 
-    models_str = models_str.replace('{used_prompts_template}', models[ai_model]["used_prompts_template"])
-    models = json.loads(models_str)
+# Add current prompt
+prompt_str = data["txt2txt"]["prompt"].replace('\n', ' ')
+models_str = models_str.replace('{prompt}', prompt_str)
+models = json.loads(models_str)
 
-    # Add current prompt
-    prompt_str = data["txt2txt"]["prompt"].replace('\n', ' ')
-    models_str = models_str.replace('{prompt}', prompt_str)
-    models = json.loads(models_str)
+ctx = models[ai_model]["prompt_template"]
 
-    ctx = models[ai_model]["prompt_template"]
+print('\nUsing helper: ' + models[ai_model]['helper'], flush=True)
 
-    print('\nUsing helper: ' + models[ai_model]['helper'], flush=True)
-
-    helper = importlib.import_module(f"txt2txt-helpers.{models[ai_model]['helper']}")
-    result = helper.run(models[ai_model], ctx)
-
-    with open(path.join(cfg_folder, 'prompts', f'prompt-{num}.txt'), 'w') as file:
-        file.write(result)
-
-    # Restore prompts file to add new used prompts
-    with open(path.join(cfg_folder, 'models.json'), 'r') as file:
-        models = json.load(file)["txt2txt"]
+helper = importlib.import_module(f"txt2txt-helpers.{models[ai_model]['helper']}")
+result = helper.run(models[ai_model], ctx, num)
 
 # Get all prompts
 prompts_arr = []
